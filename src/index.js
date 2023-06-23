4a344e8c51aebbe70d5a4ce51ebf4344265efa18
Manuel Rivera <mukhuve@gmail.com>
2023-06-23 12:07:02 -0500

    add dotenv config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import todoRouter from "./routes/todo.routes.js";
+import { config } from "dotenv";
+config();
 
+const PORT = process.env.PORT;
 const app = express();
 
 app.use(express.json());
@@ -8,6 +11,6 @@ app.use("/", express.static("public"));
 app.use("/todo", todoRouter);
 
 // Start my server
-app.listen(3000, () => {
-  console.log("Servidor en http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Servidor en http://localhost:${PORT}`);
 });
